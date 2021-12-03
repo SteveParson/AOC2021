4d6771b851d31c3e0abdfbd310824c08348029f9
Steve Parson <k47swp@mun.ca>
2021-12-03 14:17:03 -0400

    feat: day2 in c

---
+++ day2-part2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    char line[512];
+    long x = 0, depth = 0, aim = 0;
+    FILE *file = fopen(argv[1], "r");
+
+    while(fgets(line, sizeof(line), file))
+    {
+        char *token = strtok(line, " ");
+        char direction = token[0] & ~32;
+        long value = atol(strtok(NULL, " "));
+        switch(direction)
+        {
+            case 'U':
+                aim -= value;
+                break;
+            case 'D':
+                aim += value;
+                break;
+            case 'F':
+                x += value;
+                depth += (aim*value);
+                break;
+        }
+    }
+
+    printf("%ld\n", x*depth);
+    fclose(file);
+    return 0;
+}
